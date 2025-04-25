fdb27ce28b9ee8bdb27d8b465cdd4cab044b3e68
muodilo <murindahabiodilo@gmail.com>
2025-04-25 12:43:30 +0200

    create function to render images

--- dist/index.js
+++ dist/index.js
@@ -15,11 +15,20 @@ const fetchImages = () => __awaiter(void 0, void 0, void 0, function* () {
         const data = yield fetch('https://picsum.photos/v2/list?page=1');
         images = yield data.json();
         console.log(images);
+        renderImages();
     }
     catch (error) {
         console.log('failed to fetch images');
     }
 });
 const renderImages = () => {
+    images.forEach((img) => {
+        const thumbNail = `https://picsum.photos/id/${img.id}/200/150`;
+        const imageGrid = document.createElement('div');
+        const gridImage = document.createElement('img');
+        gridImage.src = thumbNail;
+        imageGrid.appendChild(gridImage);
+        gallery === null || gallery === void 0 ? void 0 : gallery.appendChild(imageGrid);
+    });
 };
 fetchImages();
--- src/index.ts
+++ src/index.ts
@@ -17,13 +17,22 @@ const fetchImages = async()=>{
         const data = await fetch('https://picsum.photos/v2/list?page=1');
         images = await data.json();
         console.log(images);
+        renderImages()
     } catch (error) {
         console.log('failed to fetch images');
     }
 }
 
 const renderImages = ()=>{
+    images.forEach((img)=>{
+        const thumbNail = `https://picsum.photos/id/${img.id}/200/150`
+        const imageGrid = document.createElement('div');
+        const gridImage = document.createElement('img');
+        gridImage.src = thumbNail;
+        imageGrid.appendChild(gridImage);
+        gallery?.appendChild(imageGrid);
 
+    })
 }
 
 fetchImages()
